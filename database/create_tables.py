c8e4427f5443efc615b71ce24fc6a36e7b14369b
pamu0171-ctrl <pamu0171@gmail.com>
2026-07-06 04:04:17 +0900

    Update create_tables.py

--- database/create_tables.py
+++ database/create_tables.py
@@ -1,1 +1,16 @@
+from database.db import connect
 
+def create_tables():
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            racer_id INTEGER,
+            name TEXT,
+            grade TEXT,
+            branch TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
