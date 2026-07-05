d6a4cfb093e183b88b3ba0c76d7a10e4c5a4c25b
pamu0171-ctrl <pamu0171@gmail.com>
2026-07-05 12:12:23 +0900

    Update odds.py

--- database/odds.py
+++ database/odds.py
@@ -1,6 +1,13 @@
-import requests
 import pandas as pd
 
 
 def get_odds(place, race):
-    return pd.DataFrame()
+    """
+    オッズ取得用（後で中身を実装）
+    """
+    return pd.DataFrame(
+        columns=[
+            "買い目",
+            "オッズ"
+        ]
+    )
