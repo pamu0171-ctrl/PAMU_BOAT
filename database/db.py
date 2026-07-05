3aa1433172a425cfa0f83725a03d54700231f2db
pamu0171-ctrl <pamu0171@gmail.com>
2026-07-05 10:49:33 +0900

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,9 +1,11 @@
-
 import sqlite3
+import os
 
-from config import DB_PATH
-
+DB_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)),
+    "data",
+    "pamu_boat.db",
+)
 
 def connect():
-
     return sqlite3.connect(DB_PATH)
