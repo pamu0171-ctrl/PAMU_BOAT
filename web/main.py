b34aed4938ae1c9680b1aa5e39388f787b192f84
pamu0171-ctrl <pamu0171@gmail.com>
2026-07-07 04:11:37 +0900

    Update main.py

--- web/main.py
+++ web/main.py
@@ -1,13 +1,12 @@
-
 import os
 import sys
 
-# sys.path.append("/content")
 sys.path.append(
     os.path.dirname(
         os.path.dirname(__file__)
     )
 )
+
 import streamlit as st
 
 from database.query import get_ai, get_odds_table
@@ -23,10 +22,18 @@ st.title("🚤 PAMU BOAT β")
 place = st.number_input("場コード", 1, 24, 1)
 
 race = st.number_input("レース", 1, 12, 1)
-date = st.text_input("開催日（YYYYMMDD）", "20260705")
+
+date = st.text_input(
+    "開催日（YYYYMMDD）",
+    "20260705"
+)
+
 st.subheader("AI予想")
+
 df = get_ai(place, race, date)
+
 ai_diff = df.iloc[0]["AI確率%"] - df.iloc[1]["AI確率%"]
+
 if ai_diff >= 20:
     danger = "★☆☆☆☆"
     comment = "イン信頼度 高"
@@ -36,18 +43,53 @@ elif ai_diff >= 10:
 else:
     danger = "★★★★★"
     comment = "被弾妙味あり"
-st.write(f"◎ 本命：{df.iloc[0]['枠']}号艇 {df.iloc[0]['選手名']}")
+
+st.write(
+    f"◎ 本命：{df.iloc[0]['枠']}号艇 {df.iloc[0]['選手名']}"
+)
+
 st.write("🎯 推奨買い目")
-st.write(f"{df.iloc[0]['枠']}-{df.iloc[1]['枠']}-{df.iloc[2]['枠']}")
-st.write(f"{df.iloc[0]['枠']}-{df.iloc[2]['枠']}-{df.iloc[1]['枠']}")
-st.write(f"{df.iloc[1]['枠']}-{df.iloc[0]['枠']}-{df.iloc[2]['枠']}")
-st.dataframe(df, use_container_width=True, hide_index=True)
+
+st.write(
+    f"{df.iloc[0]['枠']}-{df.iloc[1]['枠']}-{df.iloc[2]['枠']}"
+)
+
+st.write(
+    f"{df.iloc[0]['枠']}-{df.iloc[2]['枠']}-{df.iloc[1]['枠']}"
+)
+
+st.write(
+    f"{df.iloc[1]['枠']}-{df.iloc[0]['枠']}-{df.iloc[2]['枠']}"
+)
+
+st.dataframe(
+    df,
+    use_container_width=True,
+    hide_index=True
+)
+
 st.subheader("🚨 被弾レーダー")
+
 st.write(f"危険度：{danger}")
 st.write(comment)
+
 st.subheader("📊 オッズ")
 
-odds = get_odds_table(place, race, date)
+head = st.radio(
+    "1着固定",
+    [1, 2, 3, 4, 5, 6],
+    horizontal=True
+)
+
+odds = get_odds_table(
+    place,
+    race,
+    date
+)
+
+odds = odds[
+    odds["買い目"].str.startswith(f"{head}-")
+]
 
 st.dataframe(
     odds,
